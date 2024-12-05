9d9485f7e645581b885910b058444c747315b3e6
BahaBakan <bahabakan7@gmail.com>
2024-12-05 22:03:23 +0300

    Saç Modelini bul Kısmı eklendi.

---
+++ Controllers/SacModeliniBulController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc; 
+
+namespace KuaforProjesi.Controllers{
+
+    public class SacModeliniBulController:Controller{
+
+        public IActionResult SacModeliniBul(){
+            return View();
+        }
+    }
+}
